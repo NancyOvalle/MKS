ff475e2972f3b6103a838b2046014c4c80e27354
Nancy <nancyovalle3123@gmail.com>
2020-10-08 17:41:27 -0500

    Se crea formulario para creacion de producto

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,7 +12,7 @@
         </div>-->
 
         <div>
-                    <form action="{{route('products.store')}}" method="post" enctype="multipart/form-data" file="true">
+                    <form action="{{ route('products.store') }}" method="POST" enctype="multipart/form-data" file="true">
                        @csrf
 	                   	<br>	
 	                   	<div class="form-group">
